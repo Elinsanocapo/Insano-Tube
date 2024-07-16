782e27da59f1db1a1db86413dd887c61fb5ce047
Umblockedgame <133471107+Umblockedgame@users.noreply.github.com>
2024-07-15 21:32:13 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 python3 -m pip install yt-dlp flask waitress requests urllib3 timeago
+python3 -m pip install --upgrade yt-dlp
 python3 main.py
